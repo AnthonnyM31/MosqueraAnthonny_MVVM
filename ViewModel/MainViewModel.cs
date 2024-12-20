dafe73191b1a117a7808429ff78d511804b1c503
AnthonnyM31 <anthonnymosquera1482@gmail.com>
2024-12-20 17:27:49 -0500

    creacion de la carpeta viewmodels

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosqueraAnthonny_TareaApuntes.ViewModel
+{
+    internal class MainViewModel
+    {
+
+    }
+}
